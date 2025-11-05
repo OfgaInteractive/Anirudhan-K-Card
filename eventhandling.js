22783ee74d8a62621068b780c9652f8576416f56
Logan\Vishnu <sugumarvishnu2@gmail.com>
2025-11-06 01:45:04 +0530

    Content Hub

--- eventhandling.js
+++ eventhandling.js
@@ -53,7 +53,7 @@ function log(msg) {
         //log("Scale complete → Moving left...");
         logoModel.setAttribute("animation__move", {
           property: "position",
-          to: "-1 0 0",
+          to: "0 1 0",
           dur: 1000,
           easing: "easeInOutSine"
         });
